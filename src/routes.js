dc99b931fcce368d5614d080b6c9c31b10e638bd
thalysson1987 <thalyssonwg.nascimento@gmail.com>
2019-08-05 22:16:17 -0300

    implementando eslint, prettier e editorconfig

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-import {Router} from 'express';
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res)=>{
-    return res.json({message: 'API 1.0 Meetapp'});
-})
+routes.get('/', (req, res) => {
+  return res.json({ message: 'API 1.0 Meetapp' });
+});
 
 export default routes;
