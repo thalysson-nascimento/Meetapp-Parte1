1abda2812d78b3513771a0e13f9384a53ba6e9c3
thalysson1987 <thalyssonwg.nascimento@gmail.com>
2019-08-06 17:00:54 -0300

    identando arquivos app.js e routes.js com eslint

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-  return res.json({ message: 'API 1.0 Meetapp' });
+    return res.json({ message: 'API 1.0 Meetapp' });
 });
 
 export default routes;
